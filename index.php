8b1cf5cebb4d89d579e51b0f0dd67bc7f2b03942
Rodriguez Geoffrey <rodriguezgeoffrey.becode@gmail.com>
2020-03-03 09:51:41 +0100

    sanatization

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/bellota-fontfacekit/web fonts/bellota_italic_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+</head>
+
+<body>
+
+<header>
+
+    <!--Navbar-->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img src="assets/img/hackers-poulette-logo.png" alt="">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#products">Produits</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+
+<!--Jumbotron-->
+
+<div class="jumbotron text-sm-center jumbo-modif">
+
+    <h1 class="display-4">Backers Poulette</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    
+  </div>
+
+</header>
+
+
+
+<!--Products-->
+
+<section id="products">
+    <p>Catégorie 1</p>
+    <div class="container">
+        <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/a344ffd10153617ebc562d9a7aa62901/7fd5d/6526e7f1-84ba-4f2c-905b-17d3fb6e01df_copy%2Bof%2Bpi4_case_angle2.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry Pi 4 Case</h5>
+            <p class="card-text">The official case for Rasperry PI 4</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/8875c8b45499606103f45bb94a9d1902/7fd5d/94e326f3-1b92-450c-9fd5-23183797d30c_desktop%2Bkit%2Bcontents.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry Pi 4 desktop Kit</h5>
+            <p class="card-text">Full desktop computer kit. Just connect to HDMI display(s)</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/f1682eef7da7e8d989662d147f48977c/7fd5d/f532739a-171e-4aa0-b9f3-d05e20710b69_raspberry-pi-4-model-b.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry PI 4 model B</h5>
+            <p class="card-text">Your tiny, dual-display, desktop&nbsp;computer
+                …and robot brains, smart home hub, media centre, networked AI core, factory controller, and much more.</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<section>
+    <p>Catégorie 2</p>
+    <div class="container">
+        <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/a344ffd10153617ebc562d9a7aa62901/7fd5d/6526e7f1-84ba-4f2c-905b-17d3fb6e01df_copy%2Bof%2Bpi4_case_angle2.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry Pi 4 Case</h5>
+            <p class="card-text">The official case for Rasperry PI 4</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/8875c8b45499606103f45bb94a9d1902/7fd5d/94e326f3-1b92-450c-9fd5-23183797d30c_desktop%2Bkit%2Bcontents.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry Pi 4 desktop Kit</h5>
+            <p class="card-text">Full desktop computer kit. Just connect to HDMI display(s)</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/f1682eef7da7e8d989662d147f48977c/7fd5d/f532739a-171e-4aa0-b9f3-d05e20710b69_raspberry-pi-4-model-b.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry PI 4 model B</h5>
+            <p class="card-text">Your tiny, dual-display, desktop&nbsp;computer
+                …and robot brains, smart home hub, media centre, networked AI core, factory controller, and much more.</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<section>
+    <p>Catégorie 3</p>
+    <div class="container">
+        <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/a344ffd10153617ebc562d9a7aa62901/7fd5d/6526e7f1-84ba-4f2c-905b-17d3fb6e01df_copy%2Bof%2Bpi4_case_angle2.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry Pi 4 Case</h5>
+            <p class="card-text">The official case for Rasperry PI 4</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/8875c8b45499606103f45bb94a9d1902/7fd5d/94e326f3-1b92-450c-9fd5-23183797d30c_desktop%2Bkit%2Bcontents.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry Pi 4 desktop Kit</h5>
+            <p class="card-text">Full desktop computer kit. Just connect to HDMI display(s)</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/f1682eef7da7e8d989662d147f48977c/7fd5d/f532739a-171e-4aa0-b9f3-d05e20710b69_raspberry-pi-4-model-b.jpg" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Rasperry PI 4 model B</h5>
+            <p class="card-text">Your tiny, dual-display, desktop&nbsp;computer
+                …and robot brains, smart home hub, media centre, networked AI core, factory controller, and much more.</p>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-block btn-primary">More info</button>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+
+
+<!--Services-->
+
+ 
+
+<!--Contact-->
+
+<?php
+$action=$_REQUEST['action'];
+
+?>
+
+<form action ="" method="POST" enctype="multipart/form-data">
+
+  <input type ="hidden" name="action" value="submit">
+  Your Name: <br>
+  <input name="name" type="text" value="" size="25"/> <br>
+  Your Email: <br>
+  <input name="email" type="text" value="" size="25"/><br>
+  Your Country: <br>
+  <select name="country" size="1">
+    <option></option>
+    <option>Belgique</option>
+    <option>France</option>
+    <option>Russie</option>
+    <option>Suède</option>
+  </select><br>
+  Your message: <br>
+  <textarea name="message" rows="7" cols="30"></textarea><br>
+  <input type="submit" value="Send email"/> <br>
+
+
+
+<?php
+
+$options = array(
+  'name' => FILTER_SANITIZE_STRING,
+  'email' => FILTER_SANITIZE_EMAIL,
+  'message' => FILTER_SANITIZE_STRING);
+
+$result = filter_input_array(INPUT_POST, $options);
+
+if ($result != null AND $result != FALSE) {
+
+	echo "Tous les champs ont été nettoyés !";
+
+} else {
+
+	echo "Un champ est vide ou n'est pas correct!";
+
+}
+
+foreach($options as $key => $value)
+{
+  $result[$key]=trim($result[$key]);
+}
+
+if (isset($_POST['name'])) {
+
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$country=$_REQUEST['country'];
+$message=$_REQUEST['message'];
+
+if (($name=="")||($email=="")||($country=="")||($message==""))
+{
+echo "Veuillez compléter tous les champs";  
+}
+else
+
+echo "<script>alert('Coucou' + '$name' + '$email');</script>";
+
+}
+
+echo $result['name'];
+echo $result['email'];
+echo $country;
+echo $result['message'];
+
+
+?>
+
+</form>
+<footer>
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
+
+    <!-- Footer Elements -->
+    <div class="container">
+  
+      <!-- Grid row-->
+      <div class="row">
+  
+        <!-- Grid column -->
+        <div class="col-md-12 py-5">
+          <div class="mb-5 flex-center">
+  
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Pinterest-->
+            <a class="pin-ic">
+              <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+            </a>
+          </div>
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row-->
+  
+    </div>
+    <!-- Footer Elements -->
+  
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
+
+
+
+</footer>
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    
+</body>
+
+</html>
