ff4bb756cc5a07c890a6f3aa685012a70b5a9854
Rodriguez Geoffrey <rodriguezgeoffrey.becode@gmail.com>
2020-03-03 13:44:41 +0100

    lal

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 
 <div class="jumbotron text-sm-center jumbo-modif">
 
-    <h1 class="display-4">Backers Poulette</h1>
+    <h1 class="display-4">Hackers Poulet</h1>
     <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <hr class="my-4">
     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
@@ -140,7 +140,7 @@
 <section>
     <p>Catégorie 3</p>
     <div class="container">
-        <div class="card-deck md-3 text-center">
+        <div class="card-deck mb-3 text-center">
         <div class="card mb-4 box-shadow">
             <img class="card-img-top" src="https://www.raspberrypi.org/homepage-9df4b/static/a344ffd10153617ebc562d9a7aa62901/7fd5d/6526e7f1-84ba-4f2c-905b-17d3fb6e01df_copy%2Bof%2Bpi4_case_angle2.jpg" alt="Card image cap">
             <div class="card-body">
@@ -180,14 +180,41 @@
 
 <!--Services-->
 
-<!--Contact-->
-<div class="container text-center border border-dark">
-  <?php
-$action=$_REQUEST['action'];
+<main>
+        <div class="container" id="service">
+            <div class="row">
+                <div class="col-sm bg-dark text-white">
+                    <h2>Service 1</h2>
+                    <p>nous proposons...</p>
+                </div>
+                <div class="col-sm text-center">
+                    <img class="img-fluid" src="https://www.raspberrypi.org/homepage-9df4b/static/b74d537a2af374e986f6d3d8dc3640cc/7fd5d/1b7d602e46d47ed9f540f364bb3fbf1985b10164_red_white-mouse.jpg" alt="Generic placeholder image">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm text-center">
+                    <img class="img-fluid" src="https://www.raspberrypi.org/homepage-9df4b/static/1a8c2dea858d9a09b7382f569582a8c3/7fd5d/76d43bab-d6e5-479f-a31e-bea771589ed1_uk_white-.jpg" alt="Generic placeholder image">
+                </div>
+                <div class="col-sm bg-ligth text-black">
+                    <h2>Service 2</h2>
+                    <p>nous reproposons...</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm bg-dark text-white">
+                    <h2>Service 3</h2>
+                    <p>nous rereproposons...</p>
+                </div>
+                <div class="col-sm text-center">
+                    <img class="img-fluid" src="https://www.raspberrypi.org/homepage-9df4b/static/018189b96c524db50ebdd0f7210caad0/7fd5d/4b874a4fc4fa904ab20c988f9775889c278dad27_power-supply-1-1393x1080.jpg" alt="Generic placeholder image">
+                </div>
+            </div>
+    </main>
 
-<<<<<<< Updated upstream
-?>
-=======
+ 
+
+<!--Contact-->
+
 <div class="container text-center border border-dark" id="Contact">
   <div id="mail-request"></div>
 
@@ -195,45 +222,51 @@ $action=$_REQUEST['action'];
   <label>Your first name</label><span id="firstnameinfo" class="info"></span><br/>
   <input type="text" name="firstname" id="firstname" class="infoinput"></span>
   </div>
->>>>>>> Stashed changes
 
-<form action ="" method="POST" enctype="multipart/form-data">
+  <div>
+  <label>Your last name</label><span id="lastnameinfo" class="info"></span><br/>
+  <input type="text" name="lastname" id="lastname" class="infoinput"></span>
+  </div>
 
-  <input type ="hidden" name="action" value="submit">
-  Your Name: <br>
-  <input name="name" type="text" value="" size="25"/> <br>
-  Your Email: <br>
-  <input name="email" type="text" value="" size="25"/><br>
-  Your Country: <br>
-  <select name="country" size="1">
+  <div>
+  <label>Your email</label><span id="emailinfo" class="info"></span><br/>
+  <input type="text" name="email" id="email" class="infoinput"></span>
+  </div>
+
+  <div>
+  <label>Your Country: </label><span id="countryinfo" class="info"></span><br/>
+  <select name="country" id="country" class="infoinput" size="1">
     <option></option>
     <option>Belgique</option>
     <option>France</option>
     <option>Russie</option>
     <option>Suède</option>
-  </select><br>
-  Your message: <br>
-  <textarea name="message" rows="7" cols="30"></textarea><br>
-  <input class="btn btn-lg btn-primary my-3" type="submit" value="Send email"/> <br>
+  </select><br/>
+  </div>
 
+  <div>
+  <label>Homme</label><span id="genderm" class="info"></span>
+  <input type="checkbox" name="genderm" id="genderm" class="infoinput"></span>
+  <label>Femme</label><span id="genderf" class="info"></span>
+  <input type="checkbox" name="genderf" id="genderf" class="infoinput"></span>
+  </div>
 
-<<<<<<< Updated upstream
-=======
   
   <div>
   <label>Your Subject: </label><span id="subjectinfo" class="info"></span><br/>
   <select name="subject" id="subject" class="infoinput" size="1">
     <option></option>
-    <option>élégie</option>
+    <option>plainte</option>
     <option>bramement</option>
     <option>dénonciation</option>
   </select><br/>
   </div>
->>>>>>> Stashed changes
 
+  <div>
+  <label>Content</label><span id="content" class="info"></span><br/>
+  <textarea name="content" id="content" class="infoinput" cols="40" row="4"></textarea>
+  </div>
 
-<<<<<<< Updated upstream
-=======
 <div>
   <button name="submit" class="btnaction btn btn-lg btn-primary my-3" onClick="sendContact();">Submit</button>
 </div>
@@ -293,69 +326,11 @@ echo $result['message'];
 
 
 ?>
->>>>>>> Stashed changes
 </div>
-<div class="container text-center bg-dark text-white my-3 py-2">
-  <?php
-
-$options = array(
-  'name' => FILTER_SANITIZE_STRING,
-  'email' => FILTER_SANITIZE_EMAIL,
-  'message' => FILTER_SANITIZE_STRING);
-
-$result = filter_input_array(INPUT_POST, $options);
-
-if ($result != null AND $result != FALSE) {
-  echo "<p>";
-  echo "Tous les champs ont été nettoyés !";
-  echo "</p>";
-
-} else {
-
-	echo "Un champ est vide ou n'est pas correct!";
-
-}
-
-foreach($options as $key => $value)
-{
-  $result[$key]=trim($result[$key]);
-}
-
-if (isset($_POST['name'])) {
-
-$name=$_REQUEST['name'];
-$email=$_REQUEST['email'];
-$country=$_REQUEST['country'];
-$message=$_REQUEST['message'];
-
-if (($name=="")||($email=="")||($country=="")||($message==""))
-{
-  echo "<p>";
-  echo "Veuillez compléter tous les champs !";
-  echo "</p>";
-}
-else
-echo "<p>";
-echo "<script>alert('Coucou' + '$name' + '$email');</script>";
-echo "</p>";
-}
-
-echo $result['name'];
-echo $result['email'];
-echo $country;
-echo $result['message'];
-
-
-?>
-</div>
-
-
-</form>
-<footer>
-
-
 
 <!-- Footer -->
+
+<footer>
 <footer class="page-footer font-small cyan darken-3">
 
     <!-- Footer Elements -->
@@ -428,6 +403,8 @@ echo $result['message'];
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
+    <script src="assets/js/script.js"></script>
+
     
 </body>
 
